f28af9fc5564bf5fbb24a10a3bff831df4046a42
Deo Andrianto Iskandar <111202012699@mhs.dinus.ac.id>
2023-02-22 09:31:40 +0700

    feat: routing

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
+        <RouterLink :to="{name:'list'}">List</RouterLink>
       </nav>
     </div>
   </header>
---
+++ src/views/ListView.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+    name: 'ListView',
+    data:()=>({
+        list:[{
+            name: 'List Pertama'
+        },
+    {
+        name: 'List Kedua'
+    }
+    ]
+    })
+}
+</script>
+<template>
+    <div>
+        <h1>This is a list page</h1>
+        <ol>
+            <template v-for ="(item,index) in list" :key="index">
+                <li>
+                    {{item.name}}
+                </li>
+            </template>
+        </ol>
+    </div>
+</template>
+<style>
+
+</style>
