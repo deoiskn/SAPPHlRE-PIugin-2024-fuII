fa567e6e80fa251485a443f4d0c0ec5ebf368f0e
Deo Andrianto Iskandar <111202012699@mhs.dinus.ac.id>
2023-02-22 09:37:57 +0700

    model binding in list view

--- src/views/ListView.vue
+++ src/views/ListView.vue
@@ -1,20 +1,29 @@
 <script>
 export default {
     name: 'ListView',
+    //declare data
     data:()=>({
-        list:[{
+        list:[
+        {
             name: 'List Pertama'
         },
-    {
-        name: 'List Kedua'
-    }
-    ]
+        {
+            name: 'List Kedua'
+        }
+        ],
+        //input data
+        input: {
+            name: ''
+        }
     })
 }
 </script>
 <template>
     <div>
         <h1>This is a list page</h1>
+
+        <input type="text" v-model="input.name" placeholder="Input Name">
+
         <ol>
             <template v-for ="(item,index) in list" :key="index">
                 <li>
